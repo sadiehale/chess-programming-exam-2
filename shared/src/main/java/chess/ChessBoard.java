4ab71341bbd1ead34e4def3772e4bd4795e64f85
Sadie Hale <sadie.m.hale@gmail.com>
2025-05-12 19:45:43 -0600

    just misunderstanding two small concepts

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,7 +10,28 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "squares=" + Arrays.toString(squares) +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
+    private ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,7 +43,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +54,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow() - 1][position.getColumn() - 1];
+
     }
 
     /**
@@ -38,6 +63,33 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[8][8];
+        //pawn
+        for (int i = 0; i < 8; i++){
+            squares[1][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            squares[6][i] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+        }
+        //rook
+        squares[0][0] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        squares[0][7] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        squares[7][0] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        squares[7][7] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        //knight
+        squares[0][1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        squares[0][6] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        squares[7][1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        squares[7][6] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        //bishop
+        squares[0][2] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        squares[0][5] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        squares[7][2] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        squares[7][5] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        //queen
+        squares[0][3] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        squares[7][3] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        //king
+        squares[0][4] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        squares[7][4] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +11,33 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceType == that.pieceType && teamColor == that.teamColor;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceType, teamColor);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceType=" + pieceType +
+                ", teamColor=" + teamColor +
+                '}';
+    }
+
+    private final ChessPiece.PieceType pieceType;
+    private final ChessGame.TeamColor teamColor;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.teamColor = pieceColor;
+        this.pieceType = type;
     }
 
     /**
@@ -29,14 +56,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return teamColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
@@ -47,6 +74,193 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> movePossibilities = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+        if (pieceType == PieceType.PAWN){
+            int [][] pawnMoves;
+            int [][] pawnMoves_capt;
+            if (teamColor == ChessGame.TeamColor.WHITE){
+                pawnMoves_capt = new int[][]{{+1,+1}, {+1,-1}};
+                if(row == 2){
+                    pawnMoves = new int[][] {{+1,0}, {+2,0}};
+                }else{
+                    pawnMoves = new int[][] {{+1,0}};
+                }
+            }else{
+                pawnMoves_capt = new int[][] {{-1,-1}, {-1,+1}};
+                if (row == 7){
+                    pawnMoves = new int[][] {{-1,0}, {-2,0}};
+                }else{
+                    pawnMoves = new int[][] {{-1,0}};
+                }
+            }
+
+            for (int[] possibility : pawnMoves){
+                int newRow = row;
+                newRow += possibility[0];
+                int newCol = col;
+                newCol += possibility[1];
+                if(inBounds(newRow, newCol)){
+                    ChessPosition newSpot = new ChessPosition(newRow,newCol);
+                    if(row == 1 && teamColor == ChessGame.TeamColor.WHITE){
+                        if(board.getPiece(newSpot) == null){
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.QUEEN));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.ROOK));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.KNIGHT));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.BISHOP));
+                        }
+                    }else if(row == 8 && teamColor == ChessGame.TeamColor.BLACK) {
+                        if(board.getPiece(newSpot)== null){
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.QUEEN));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.ROOK));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.KNIGHT));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.BISHOP));
+                        }
+                    }else{
+                        movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                    }
+                }
+            }
+            for (int[] possibility : pawnMoves_capt){
+                int newRow = row + possibility[0];
+                int newCol = col + possibility[1];
+                if(inBounds(newRow, newCol)){
+                    ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                    if(row == 1 && teamColor == ChessGame.TeamColor.WHITE){
+                        if(board.getPiece(newSpot).teamColor != board.getPiece(myPosition).teamColor){
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.QUEEN));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.ROOK));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.KNIGHT));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.BISHOP));
+                        }
+                    }else if(row == 8 && teamColor == ChessGame.TeamColor.BLACK) {
+                        if (board.getPiece(newSpot).teamColor != board.getPiece(myPosition).teamColor) {
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.QUEEN));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.ROOK));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.KNIGHT));
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, PieceType.BISHOP));
+                        }
+                    }
+                }
+            }
+        }else if(pieceType == PieceType.ROOK){
+            int [][] rookMoves = {
+                    {+1,0}, {-1,0}, {0,+1}, {0,-1}
+            };
+            for (int[] possibility : rookMoves){
+                int newRow = row;
+                int newCol = col;
+                while(true){
+                    if(!inBounds(newRow, newCol)){
+                        break;
+                    }
+                    newRow += possibility[0];
+                    newCol += possibility[1];
+                    if (inBounds(newRow, newCol)) {
+                        ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                        if (canMove(newSpot, board)) {
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                        }else{
+                            break;
+                        }
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }else if(pieceType == PieceType.KNIGHT){
+            int [][] knightMoves = {
+                    {+2, +1}, {+2,-1}, {-2, +1}, {-2, -1}, {+1, +2}, {+1, -2}, {-1, +2}, {-1, -2}
+            };
+            for (int[] possibility : knightMoves){
+                int newRow = row + possibility[0];
+                int newCol = col + possibility[1];
+                if(inBounds(newRow, newCol)){
+                    ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                    if (canMove(newSpot, board)){
+                        movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                    }
+                }
+            }
+        }else if(pieceType == PieceType.BISHOP){
+            int[][] bishopMoves = {
+                    {+1,+1}, {+1, -1}, {-1, +1}, {-1,-1}
+            };
+            for(int[] possibility : bishopMoves){
+                int newRow = row;
+                int newCol = col;
+                while(true){
+                    if (!inBounds(newRow, newCol)){
+                        break;
+                    }
+                    newRow += possibility[0];
+                    newCol += possibility[1];
+                    if (!inBounds(newRow, newCol)){
+                        break;
+                    }else{
+                        ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                        if(canMove(newSpot, board)){
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                        }else{
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }else if(pieceType == PieceType.QUEEN){
+            int[][] queenMoves = {
+                    {+1, +1}, {+1, 0}, {+1, -1}, {-1, -1}, {-1, 0}, {-1, +1}, {0, +1}, {0, -1}
+            };
+            for(int[] possibility : queenMoves){
+                int newRow = row;
+                int newCol = col;
+                while(true){
+                    if (!inBounds(newRow, newCol)){
+                        break;
+                    }
+                    newRow += possibility[0];
+                    newCol += possibility[1];
+                    if (!inBounds(newRow, newCol)){
+                        break;
+                    }else{
+                        ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                        if(canMove(newSpot, board)){
+                            movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                        }else{
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }else if(pieceType == PieceType.KING){
+            int [][] kingMoves = {
+                    {+1, +1}, {+1, 0}, {+1, -1}, {-1, -1}, {-1, 0}, {-1, +1}, {0, +1}, {0, -1}
+            };
+            for (int[] possibility : kingMoves){
+                int newRow = row + possibility[0];
+                int newCol = col + possibility[1];
+                if(inBounds(newRow, newCol)){
+                    ChessPosition newSpot = new ChessPosition(newRow, newCol);
+                    if (canMove(newSpot, board)){
+                        movePossibilities.add(new ChessMove(myPosition, newSpot, null));
+                    }
+                }
+            }
+        }
+        return movePossibilities;
+    }
+    public boolean inBounds(int row, int col){
+        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+    }
+    public boolean canMove(ChessPosition newSpot, ChessBoard board){
+        if(!inBounds(newSpot.getRow(), newSpot.getColumn())) {
+           return false;
+        }
+        ChessPiece spotCheck = board.getPiece(newSpot);
+        return spotCheck == null || board.getPiece(newSpot).teamColor != this.teamColor;
     }
 }
